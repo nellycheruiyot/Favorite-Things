d91825e5686a042be01fd4c6b1410081679e8c92
Nelly Cheruiyot and Nason Mangeli <nelly@>
2017-08-17 14:37:17 +0300

    Add .gitignore file 	Add .DS_Store Deleted .DS_Store Modify index.html to list out favorite things Modify scripts.js to add arrays

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,13 @@ $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var favorites = [$("input#TvShow").val(), $("input#DogBreed").val(), $("input#Food").val(), $("input#ComputerType").val()];
 
-    $(".TvShow").append(favorites[0]);
-    $(".DogBreed").append(favorites[1]);
-    $(".Food").append(favorites[2]);
-    $(".ComputerType").append(favorites[3]);
+    var somefavorites= [];
+    somefavorites.push(favorites[1], favorites[0], favorites[2]);
+
+    $(".DogBreed").append(somefavorites[0]);
+    $(".TvShow").append(somefavorites[1]);
+    $(".Food").append(somefavorites[2]);
+    //$(".ComputerType").append(favorites[3]);
 
     $("#story").show();
 
